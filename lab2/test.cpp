2e70eefb9fde466557a33b39509d563e0caef7b3
Марк Бухаров <mbukharov52@gmail.com>
2024-05-23 22:58:22 +0300

    Update test.cpp

--- lab2/test.cpp
+++ lab2/test.cpp
@@ -9,6 +9,7 @@ TEST(test, Insert) {
     tree = insert(tree, node2);
     char res1[200] = "";
     PrintTree(tree, res1);
+    EXPECT_EQ(tree->child[0]->child[0]->value, 3);
     EXPECT_STREQ(res1, "5 (4 (3 () ()) ()) ()");
 }
 
@@ -24,6 +25,7 @@ TEST(test, Find) {
     char res22[200] = "";
     PrintTree(find4, res24);
     PrintTree(find2, res22);
+    EXPECT_EQ(find4->child[1]->value, 5);
     EXPECT_STREQ(res24, "4 () (5 () ())");
     EXPECT_STREQ(res22, "");
 }
@@ -40,6 +42,7 @@ TEST(test, Split) {
     PrintTree(splitTree[0], res31);
     PrintTree(splitTree[1], res32);
     EXPECT_STREQ(res31, "3 () ()");
+    EXPECT_EQ(splitTree[1]->child[1]->value, 5);
     EXPECT_STREQ(res32, "4 () (5 () ())");
 }
 
@@ -53,6 +56,8 @@ TEST(test, Join) {
     Node* node21 = create(9);
     tree2 = insert(tree2, node21);
     char res4[200] = "";
-    PrintTree(join(tree, tree2), res4);
+    Node* joined = join(tree, tree2);
+    PrintTree(joined, res4);
+    EXPECT_EQ(joined->child[1]->child[0]->value, 7);
     EXPECT_STREQ(res4, "5 (4 (3 () ()) ()) (9 (7 () ()) ())");
 }
